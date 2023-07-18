5e915ac52d875ff7dca8f2449ee9e6f00450ad69
sondeokju <thsejrwn78@naver.com>
2023-07-18 15:03:39 +0900

    5.0 Login

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { LoginInput } from './dtos/login.dto';
 
 @Injectable()
 export class UsersService {
@@ -31,7 +32,45 @@ export class UsersService {
       // make error
       return [false, "Couldn't create account"];
     }
-
     // create user & hash the password
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    try {
+      const user = await this.users.findOne({
+        where: {
+          email: email,
+        },
+      });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'lalalala',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+
+    // check if the password is correct
+    // make a JWT and give it to the user
   }
 }
